2cb7744918cbf4788944166970700c872cdc3ed7
Volodymyr Baisa <biosvova@gmail.com>
2021-01-08 17:34:23 -0500

    add coments

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,8 +2,10 @@ const path = require("path");
 const router = require("express").Router();
 const apiRouter = require("./api");
 
+//Api routes
 router.use("/api", apiRouter);
 
+//HTML routes
 router.use((req, res) => {
     res.sendFile(path.join(__dirname, "../../client/build/index.html"));
 });
