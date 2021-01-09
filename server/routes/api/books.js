2004dad511dafd3aafff034d488e1b980536d6a5
Volodymyr Baisa <biosvova@gmail.com>
2021-01-08 19:06:20 -0500

    add model and api all books

---
+++ server/routes/api/books.js
@@ -0,0 +1,12 @@
+const router = require("express").Router();
+const axios = require("axios");
+
+const Book = require("../../models/book");
+
+router.get("/", (req, res) => {
+    Book.find()
+        .then((book) => {
+            res.json(book);
+        })
+        .catch((err) => res.json({ error: err }));
+});
