8aaebafffa9d612eec0e223c8c978951f9513e08
Volodymyr Baisa <biosvova@gmail.com>
2021-01-08 23:21:19 -0500

    add MessageBox and code message Alert in Books

--- client/src/pages/Books.js
+++ client/src/pages/Books.js
@@ -1,3 +1,27 @@
 import React, { Component } from "react";
 
-import API from "../utils/API";
+import API from "../helpers/API";
+
+export default class Books extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            searchValue: "",
+            books: [],
+            notif: {
+                isActive: false,
+                type: "",
+                message: "",
+            },
+        };
+    }
+
+    handleChange(e) {
+        e.preventDefault();
+        this.setState({ searchValue: e.target.value });
+    }
+
+    render() {
+        return <div className="Book"></div>;
+    }
+}
